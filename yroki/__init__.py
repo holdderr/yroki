8ac7046489c032dac3b3c83dc3d957ecf8beeb15
holdderr <ridalkucukov43@gmail.com>
2022-12-08 15:09:42 +0500

    set

--- yroki/__init__.py
+++ yroki/__init__.py
@@ -1,13 +1,7 @@
-l = [1, 2, 7, 3, 5, 7]
-print(l)
-l.append("список")
-l.insert(1, [2])
-a = [1, 2, 4, 5, 9]
-l.append(a)
-immutable_tuple = (4, 5, 6)
-l.insert(4, immutable_tuple)
-print(l)
-l.pop(2)
-print(l[-1])
-l[0], l[1] = l[1], l[2]
-print(l)
+
+mixed_set = {2.0, "Nicholas", (1, 2, 3)}
+a = frozenset(['abcabc'])
+all_months = mixed_set.union(a)
+
+
+print(mixed_set,a,all_months)
